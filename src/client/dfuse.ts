2777e73122c80b2a3d2da0bd283b33d9a624f464
dliuproduction <dliuproduction@gmail.com>
2020-06-23 14:30:56 -0400

    new client initialize flow

--- src/client/dfuse.ts
+++ src/client/dfuse.ts
@@ -1,26 +1,8 @@
-import { DfuseClient, createDfuseClient } from "@dfuse/client"
+import { DfuseClient } from "@dfuse/client"
 
 let dfuseClient: DfuseClient
 export const getDfuseClient = () => dfuseClient
 
-export const initializeDfuseClient = (client: DfuseClient) => {
-  dfuseClient = client
-}
-
-export type ClientConfig = {
-  apiKey: string
-  network: string
-  authUrl: string
-  secure: boolean
-}
-
-export const initializeDfuseClientFromConfig = (clientConfig: ClientConfig) => {
-  initializeDfuseClient(
-    createDfuseClient({
-      apiKey: clientConfig.apiKey,
-      network: clientConfig.network,
-      authUrl: clientConfig.authUrl,
-      secure: clientConfig.secure,
-    })
-  )
+export const initializeDfuseClient = (initializedClient: DfuseClient) => {
+  dfuseClient = initializedClient
 }
