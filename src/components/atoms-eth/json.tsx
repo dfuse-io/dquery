933f5f76004f4aa0b0bdd3227f83e517b4ff77a3
dliuproduction <dliuproduction@gmail.com>
2020-07-02 12:37:39 -0400

    json atom

---
+++ src/components/atoms-eth/json.tsx
@@ -0,0 +1,41 @@
+import * as React from "react"
+import styled from "@emotion/styled"
+import { system, display, layout, typography, fontFamily } from "styled-system"
+
+const whiteSpace = system({ whiteSpace: { property: "whiteSpace" } })
+const wordWrap = system({ wordWrap: { property: "wordWrap" } })
+
+const JsonContainerCode = styled.code`
+${display}
+${layout}
+${typography}
+${fontFamily}
+${whiteSpace}
+${wordWrap}
+`
+
+JsonContainerCode.defaultProps = {
+  fontFamily: "Roboto Mono, monospace",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  display: "block",
+  overflowX: "hidden",
+}
+
+const JsonContainerPre = styled.pre`
+${display}
+${typography}
+${whiteSpace}
+`
+
+JsonContainerPre.defaultProps = {
+  whiteSpace: "pre-wrap",
+  display: "block",
+  overflowX: "hidden",
+}
+
+export const JsonWrapper: React.FC<{ fontSize?: string }> = ({ fontSize, children }) => (
+  <JsonContainerPre>
+    <JsonContainerCode fontSize={fontSize || "15px"}>{children}</JsonContainerCode>
+  </JsonContainerPre>
+)
